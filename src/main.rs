b660e9fe97a1fc6a861f59ea2ab3fe9ebb0b92d9
Yasuaki Gohko <ygohko@gmail.com>
2024-08-17 17:43:09 +0900

    Basic implementation of tax calculation.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 slint::slint! {
-    import { Button, VerticalBox } from "std-widgets.slint";
+    import {
+        Button,
+        LineEdit,
+        VerticalBox
+    } from "std-widgets.slint";
+
     export component App {
+        in-out property <int> price: 100;
+        in property <int> tax: 0;
+        in property <int> total: 0;
+        callback clicked <=> calculateButton.clicked;
+
         VerticalBox {
             Text {
                 text: "Hello, World";
+            }
+            LineEdit {
+                text: price;
+                edited => {
+                    price = self.text.to-float();
+                }
+            }
+            LineEdit {
+                text: tax;
+            }
+            LineEdit {
+                text: total;
+            }
+            calculateButton := Button {
+                text: "Calculate";
             }
         }
     }
@@ -11,5 +36,15 @@ slint::slint! {
 
 fn main() {
     let app = App::new().unwrap();
+    let weak = app.as_weak();
+    app.on_clicked(move || {
+        let app = weak.upgrade().unwrap();
+        let price = app.get_price();
+        println!("price: {}", price);
+        let tax = price / 10;
+        let total = price + tax;
+        app.set_tax(tax);
+        app.set_total(total);
+    });
     app.run().unwrap();
 }
